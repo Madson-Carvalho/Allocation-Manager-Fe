81b5962b44add7c898c9ac87524fb5d88d2d920f
GABRIEL MONTEIRO DE SOUZA <gabriel_m_de-souza@estudante.sc.senai.br>
2024-06-28 20:51:45 -0300

    comit para fazer o merge da master

--- src/components/ganttCharts/GanttCharts.js
+++ src/components/ganttCharts/GanttCharts.js
@@ -1,21 +1,7 @@
 import './GanttCharts.css';
 import {Chart} from 'react-google-charts';
 
-export default function GanttCharts({columns, projects}) {
-
-    const rows = ([
-        projects[0].name,
-        projects[0].projectCoordinator,
-        new Date(projects[0].project?.initialDate),
-        new Date(projects[0].project?.deliveryDate),
-        null,
-        null,
-        ""
-    ]);
-
-    console.log(columns)
-    console.log(rows);
-
+export default function GanttCharts({columns, rows}) {
     const data = [columns, ...rows];
 
     const options = {
--- src/pages/informations/Informations.js
+++ src/pages/informations/Informations.js
@@ -2,53 +2,57 @@ import './Informations.css';
 import BasePage from "../../components/basePage/BasePage";
 import GanttCharts from '../../components/ganttCharts/GanttCharts';
 import httpGet from "../../utils/httpRequest/httpGet";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
+import CustomSelect from "../../components/customSelect/CustomSelect";
 
 const Informations = () => {
-  const [data, setData] = useState(null);
-  const [projeto, setProjeto] = useState(null);
+    const [data, setData] = useState(null);
+    const [project, setProject] = useState(null);
+    const [rows, setRows] = useState(null);
 
-  useEffect(() => {
-    httpGet("projects/find-all", setData);
-  }, []);
+    useEffect(() => {
+        httpGet("projects/find-all", setData);
+    }, []);
 
-  useEffect(() => {
-    if (data && data.length > 0) {
-      setProjeto(data[0]);
-    }
-  }, [data]);
+    const columns = [
+        {type: "string", label: "Task Name"},
+        {type: "string", label: "Task ID"},
+        {type: "date", label: "Start Date"},
+        {type: "date", label: "End Date"},
+        {type: "number", label: "Duration"},
+        {type: "number", label: "Percent Complete"},
+        {type: "string", label: "Dependencies"}
+    ];
 
-  const columns = [
-    { type: "string", label: "Task Name" },
-    { type: "string", label: "Task ID" },
-    { type: "date", label: "Start Date" },
-    { type: "date", label: "End Date" },
-    { type: "number", label: "Duration" },
-    { type: "number", label: "Percent Complete" },
-    { type: "string", label: "Dependencies" }
-  ];
+    useEffect(() => {
+        if(project){
+            const rows = ([
+                project?.name,
+                project?.projectCoordinator,
+                new Date(project?.initialDate),
+                new Date(project?.deliveryDate),
+                null,
+                null,
+                ""
+            ]);
+            setRows(rows)
+        }
 
-  const handleProjetoChange = (event) => {
-    const projetoSelecionado = data.find(p => p.id === parseInt(event.target.value));
-    setProjeto(projetoSelecionado);
-  };
+    }, [project]);
 
-  return (
-    <>
-      <BasePage title={projeto?.name}>
-        {data && (
-          <>
-            <select onChange={handleProjetoChange} value={projeto?.id}>
-              {data.map(projeto => (
-                <option key={projeto.id} value={projeto.id}>{projeto.name}</option>
-              ))}
-            </select>
-            <GanttCharts columns={columns} projects={data} />
-          </>
-        )}
-      </BasePage>
-    </>
-  );
+    const handleProjetoChange = (event) => {
+        const projetoSelecionado = data.find(p => p.id === parseInt(event.target.value));
+        setProject(projetoSelecionado);
+    };
+
+    return (
+        <>
+            <BasePage title={project && project?.name}>
+                {data && <CustomSelect label="" options={data} required={true} value={project?.id} onChange={handleProjetoChange} name="teste"/>}
+                {rows && <GanttCharts columns={columns} rows={rows}/>}
+            </BasePage>
+        </>
+    );
 };
 
 export default Informations;
