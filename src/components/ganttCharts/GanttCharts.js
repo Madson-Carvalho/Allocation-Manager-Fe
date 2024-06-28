08db8659fd31519be18540dc68be56d37018e582
monteiro-gabriel <gabriel.monteiro@cflow.com.br>
2024-06-27 22:23:26 -0300

    alterado gantt e adiconado dados do backend

--- src/components/ganttCharts/GanttCharts.js
+++ src/components/ganttCharts/GanttCharts.js
@@ -1,141 +1,20 @@
 import './GanttCharts.css';
 import {Chart} from 'react-google-charts';
 
-export default function GanttCharts() {
+export default function GanttCharts({columns, projects}) {
 
-    const columns = [
-        {type: "string", label: "Task ID"},
-        {type: "string", label: "Task Name"},
-        {type: "string", label: "Qualification"},
-        {type: "date", label: "Start Date"},
-        {type: "date", label: "End Date"},
-        {type: "number", label: "Duration"},
-        {type: "number", label: "Percent Complete"},
-        {type: "string", label: "Dependencies"},
-    ];
+    const rows = ([
+        projects[0].name,
+        projects[0].projectCoordinator,
+        new Date(projects[0].project?.initialDate),
+        new Date(projects[0].project?.deliveryDate),
+        null,
+        null,
+        ""
+    ]);
 
-    const rows = [
-        [
-            "2024Spring",
-            "João cleber",
-            "back-end",
-            new Date(2024, 2, 22),
-            new Date(2024, 5, 20),
-            null,
-            100,
-            null,
-        ],
-        [
-            "2024Summer",
-            "Madson Carvalho",
-            "back-end",
-            new Date(2024, 5, 21),
-            new Date(2024, 8, 20),
-            null,
-            100,
-            null,
-        ],
-        [
-            "2024Autumn",
-            "Gabriel Monteiro",
-            "back-end",
-            new Date(2024, 8, 21),
-            new Date(2024, 11, 20),
-            null,
-            100,
-            null,
-        ],
-        [
-            "2024Winter",
-            "Rodrigo Dondé",
-            "back-end",
-            new Date(2024, 11, 21),
-            new Date(2025, 2, 21),
-            null,
-            100,
-            null,
-        ],
-        [
-            "2025Spring",
-            "Mayra Carvalho",
-            "back-end",
-            new Date(2025, 2, 22),
-            new Date(2025, 5, 20),
-            null,
-            50,
-            null,
-        ],
-        [
-            "2025Summer",
-            "Yuri Grams",
-            "back-end",
-            new Date(2025, 5, 21),
-            new Date(2025, 8, 20),
-            null,
-            0,
-            null,
-        ],
-        [
-            "2025Autumn",
-            "Vinicius Paiva",
-            "back-end",
-            new Date(2025, 8, 21),
-            new Date(2025, 11, 20),
-            null,
-            0,
-            null,
-        ],
-        [
-            "2025Winter",
-            "Mateus Kons",
-            "back-end",
-            new Date(2025, 11, 21),
-            new Date(2026, 2, 21),
-            null,
-            0,
-            null,
-        ],
-        [
-            "Football",
-            "Gilberto Neves",
-            "back-end",
-            new Date(2024, 8, 4),
-            new Date(2025, 1, 1),
-            null,
-            100,
-            null,
-        ],
-        [
-            "Baseball",
-            "Regis Queen",
-            "back-end",
-            new Date(2025, 2, 31),
-            new Date(2025, 9, 20),
-            null,
-            14,
-            null,
-        ],
-        [
-            "Basketball",
-            "Mauricio Ricardo",
-            "back-end",
-            new Date(2024, 9, 28),
-            new Date(2025, 5, 20),
-            null,
-            86,
-            null,
-        ],
-        [
-            "Hockey",
-            "Jorge Matos",
-            "back-end",
-            new Date(2024, 9, 8),
-            new Date(2025, 5, 21),
-            null,
-            89,
-            null,
-        ],
-    ];
+    console.log(columns)
+    console.log(rows);
 
     const data = [columns, ...rows];
 
--- src/pages/informations/Informations.js
+++ src/pages/informations/Informations.js
@@ -1,14 +1,54 @@
 import './Informations.css';
 import BasePage from "../../components/basePage/BasePage";
 import GanttCharts from '../../components/ganttCharts/GanttCharts';
-
+import httpGet from "../../utils/httpRequest/httpGet";
+import { useEffect, useState } from "react";
 
 const Informations = () => {
-    return (
-        <BasePage title={'Projeto Alpha'}>
-            <GanttCharts/>
-        </BasePage>
-    )
-}
+  const [data, setData] = useState(null);
+  const [projeto, setProjeto] = useState(null);
+
+  useEffect(() => {
+    httpGet("projects/find-all", setData);
+  }, []);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setProjeto(data[0]);
+    }
+  }, [data]);
+
+  const columns = [
+    { type: "string", label: "Task Name" },
+    { type: "string", label: "Task ID" },
+    { type: "date", label: "Start Date" },
+    { type: "date", label: "End Date" },
+    { type: "number", label: "Duration" },
+    { type: "number", label: "Percent Complete" },
+    { type: "string", label: "Dependencies" }
+  ];
+
+  const handleProjetoChange = (event) => {
+    const projetoSelecionado = data.find(p => p.id === parseInt(event.target.value));
+    setProjeto(projetoSelecionado);
+  };
+
+  return (
+    <>
+      <BasePage title={projeto?.name}>
+        {data && (
+          <>
+            <select onChange={handleProjetoChange} value={projeto?.id}>
+              {data.map(projeto => (
+                <option key={projeto.id} value={projeto.id}>{projeto.name}</option>
+              ))}
+            </select>
+            <GanttCharts columns={columns} projects={data} />
+          </>
+        )}
+      </BasePage>
+    </>
+  );
+};
 
 export default Informations;
