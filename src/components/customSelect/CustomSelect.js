7e5b126e22ed5118ca8d4e1dd715c0bce789019e
GabriellMonteiro <gabrielbrava@outlook.com.br>
2024-06-29 18:57:22 -0300

    correções para o pull request

--- src/components/customSelect/CustomSelect.js
+++ src/components/customSelect/CustomSelect.js
@@ -6,7 +6,7 @@ const CustomSelect = ({name, id, options, label, required, onChange, value}) => 
             <select name={name} id={id} value={value} required={required} onChange={onChange}>
                 <option value={''}>-- Selecione uma opção --</option>
                 {options.map(option => (
-                    <option key={option.id} value={option.value}>{option.name}</option>
+                    <option value={option.value}>{option.name}</option>
                 ))}
             </select>
             <label className="label" htmlFor={id}>{label}</label>
--- src/routes/PersonRoutes.js
+++ src/routes/PersonRoutes.js
@@ -3,14 +3,12 @@ import Home from "../pages/home/Home";
 import ListEmployees from "../pages/listEmployees/ListEmployees";
 import RegisterEmployees from "../pages/registerEmployees/RegisterEmployees";
 import Informations from "../pages/informations/Informations";
-import ListProjects from "../pages/listProjects/ListProjects";
 import RegisterProject from "../pages/registerProject/RegisterProject";
 
 const PersonRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/list-projects' element={<ListProjects/>}/>
             <Route path='/create-project' element={<RegisterProject/>}/>
             <Route path='/edit-project/:id' element={<RegisterProject/>}/>
             <Route path='/employees' element={<ListEmployees/>}/>
