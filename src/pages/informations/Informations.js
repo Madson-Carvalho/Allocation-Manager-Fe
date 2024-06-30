0659aa1f87c4ffc4f1cf3a47fabf3b1389aa9909
Madson-Carvalho <madsonraynnon@gmail.com>
2024-06-30 16:48:39 -0300

    Fazendo a associação de projeto e colaborador na tela do gantt.

--- src/pages/informations/Informations.js
+++ src/pages/informations/Informations.js
@@ -5,12 +5,23 @@ import httpGet from "../../utils/httpRequest/httpGet";
 import {useEffect, useState} from "react";
 import CustomSelect from "../../components/customSelect/CustomSelect";
 import Modal from "../../components/modal/Modal";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUserPlus} from "@fortawesome/free-solid-svg-icons";
+import Form from "../../components/form/Form";
+import {Multiselect} from "multiselect-react-dropdown";
+import multiSelectStyle from "../../utils/multiSelectStyle";
+import {ToastContainer} from "react-toastify";
+import CustomInputSubmit from "../../components/customInputSubmit/CustomInputSubmit";
+import httpPut from "../../utils/httpRequest/httpPut";
 
 const Informations = () => {
     const [data, setData] = useState(null);
     const [project, setProject] = useState(null);
     const [rows, setRows] = useState([]);
     const [showModal, setShowModal] = useState(false);
+    const [showAddEmployeeModal, setShowAddEmployeeModal] = useState(false);
+    const [selectedEmployee, setSelectEmployee] = useState([]);
+    const [employees, setEmployee] = useState([]);
 
     useEffect(() => {
         httpGet("projects/find-all", setData);
@@ -41,7 +52,7 @@ const Informations = () => {
 
     useEffect(() => {
 
-        if(project && project.employees.length === 0){
+        if (project && project.employees.length === 0) {
             setShowModal(true)
         }
 
@@ -57,7 +68,7 @@ const Informations = () => {
             ]));
             setRows(row);
             setShowModal(false);
-        }else{
+        } else {
             setRows(null);
         }
 
@@ -71,10 +82,56 @@ const Informations = () => {
 
     const closeModal = () => setShowModal(false);
 
+    const closeAddNewEmployeeModal = () => {
+        setShowAddEmployeeModal(false);
+    }
+
+    const handleClick = () => {
+        httpGet('employees/find-all', setEmployee)
+
+        setShowAddEmployeeModal(true);
+    }
+
+    const options = employees.map(employee => ({
+        name: employee.name,
+        id: employee.employeeId
+    }));
+
+    const handleSelect = (e) => {
+        setSelectEmployee(e)
+    }
+
+    const handleRemove = (e) => {
+        setSelectEmployee(selectedEmployee.filter(x => x.id !== e.id))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const employeesSet = new Set(selectedEmployee.map(selected => selected.id));
+        const employeesToSave = employees.filter(employee => employeesSet.has(employee.employeeId));
+
+        const updatedProject = {
+            ...project,
+            employees: [...project.employees, ...employeesToSave]
+        };
+
+        httpPut(`projects/update-project`, updatedProject, (response) => {
+            setProject(response);
+        });
+
+        setShowAddEmployeeModal(false);
+    }
+
     return (
         <>
             <BasePage title={"projeto em perspectiva Gantt"}>
-                {data && <CustomSelect label="Projeto" options={data} onChange={handleProjetoChange} required={true}/>}
+                <div className='gantt-options'>
+                    {data &&
+                        <CustomSelect label="Projeto" options={data} onChange={handleProjetoChange} required={true}
+                                      style={{width: '100%'}}/>}
+                    <button className='new-employee' onClick={handleClick}><FontAwesomeIcon icon={faUserPlus}/></button>
+                </div>
                 {rows && rows.length > 0 ? (
                     <GanttCharts columns={columns} rows={rows}/>
                 ) : (
@@ -82,6 +139,30 @@ const Informations = () => {
                         <h2>Não há colaboradores para este projeto.</h2>
                     </Modal>
                 )}
+                <Modal show={showAddEmployeeModal} handleClose={closeAddNewEmployeeModal}>
+                    <Form onSubmit={handleSubmit}>
+                        <Multiselect options={options}
+                                     onSelect={handleSelect}
+                                     onRemove={handleRemove}
+                                     displayValue={'name'} placeholder='Escolha um colaborador'
+                                     closeIcon='close' showArrow={true}
+                                     style={multiSelectStyle}
+                        />
+                        <CustomInputSubmit value='Adicionar' style={{width: '30%'}} disabled={!selectedEmployee.length > 0}/>
+                    </Form>
+                </Modal>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
             </BasePage>
         </>
     );
---
+++ src/utils/multiSelectStyle.js
@@ -0,0 +1,15 @@
+const multiSelectStyle = {
+    searchBox: {
+        minWidth: '35rem',
+        fontSize: '1.2rem',
+        minHeight: '3rem'
+    },
+    chips: {
+        background: '#2A63A9'
+    },
+    option: {
+        color: '#0b3b5b'
+    },
+}
+
+export default multiSelectStyle;
