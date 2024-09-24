4c097ed36b32951962c41239730ff89ec9828719
MAYRA KAUANE DA SILVA CARVALHO <mayra_carvalho@estudante.sesisenai.org.br>
2024-09-24 19:34:15 -0300

    ajuste de design da tabela

--- src/components/ReactTable/ReactTable.js
+++ src/components/ReactTable/ReactTable.js
@@ -1,11 +1,11 @@
 import './ReactTable.css';
 import React from "react";
-import {useTable, useFilters, usePagination} from "react-table";
-import {TextFilter} from "./TextFilter";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPencil, faTrash} from "@fortawesome/free-solid-svg-icons";
+import { useTable, useFilters, usePagination } from "react-table";
+import { TextFilter } from "./TextFilter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export default function ReactTable({columns, data, title, onEdit, onDelete}) {
+export default function ReactTable({ columns, data, title, onEdit, onDelete }) {
     const {
         headerGroups,
         page,
@@ -15,7 +15,7 @@ export default function ReactTable({columns, data, title, onEdit, onDelete}) {
         canNextPage,
         canPreviousPage,
         pageOptions,
-        state: {pageIndex, pageSize},
+        state: { pageIndex, pageSize },
         setPageSize
     } = useTable(
         {
@@ -28,62 +28,63 @@ export default function ReactTable({columns, data, title, onEdit, onDelete}) {
 
     return (
         <>
-        <div className="tableListSchema">
-            <table className="customTable">
-                <thead>
-                {headerGroups[0].headers.length > 0 ?
-                    <tr>
-                        <th colSpan={headerGroups[0].headers.length + 1} id="tableTitle">
-                            {title}
-                        </th>
-                    </tr> : null}
-                    <tr>
-                                <th colSpan={headerGroups[0].headers.length + 1} style={{backgroundColor: "#FF0000"}}>asdasd
-                        </th>
-                    </tr>
-                {headerGroups.map(headerGroup => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map(column => (
-                            <th key={column.id}>
-                                {column.render("Header").toUpperCase()}
-                            </th>
+            <div className="tableListSchema">
+                <table className="customTable">
+                    <thead>
+                        {headerGroups[0].headers.length > 0 ?
+                            <tr>
+                                <th colSpan={headerGroups[0].headers.length + 1} id="tableTitle">
+                                    {title}
+                                </th>
+                            </tr> : null}
+                            <tr>
+                                <th colSpan={headerGroups[0].headers.length + 1} style={{backgroundColor: "#D8D6D6", fontSize: "5px"}}>
+                                &nbsp;
+                                </th>
+                            </tr>
+                        {headerGroups.map(headerGroup => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map(column => (
+                                    <th key={column.id}>
+                                        {column.render("Header").toUpperCase()}
+                                    </th>
+                                ))}
+                                <th></th>
+                            </tr>
                         ))}
-                        <th></th>
-                    </tr>
-                ))}
-                </thead>
-                <tbody>
-                {page.map((row) => {
-                    prepareRow(row);
-                    return (
-                        <tr key={row.id}>
-                            {row.cells.map(cell => {
-                                if (Array.isArray(cell.value)) {
-                                    return <td key={cell.value.id}>{cell.value.map(x => x.name).join(', ')}</td>;
-                                }
-                                return <td key={cell.id}>{cell.render("Cell")}</td>;
-                            })}
-                            <td>
-                                <button
-                                    className='customButton'
-                                    onClick={() => onEdit(row.original)}
-                                >
-                                    <FontAwesomeIcon icon={faPencil} inverse/>
-                                </button>
-                                <button
-                                    className='customButton'
-                                    onClick={() => onDelete(row.original)}
-                                >
-                                    <FontAwesomeIcon icon={faTrash} inverse/>
-                                </button>
-                            </td>
-                        </tr>
-                    );
-                })}
-                </tbody>
-            </table>
-        </div>
-        <div className="tableFooter">
+                    </thead>
+                    <tbody>
+                        {page.map((row) => {
+                            prepareRow(row);
+                            return (
+                                <tr key={row.id}>
+                                    {row.cells.map(cell => {
+                                        if (Array.isArray(cell.value)) {
+                                            return <td key={cell.value.id}>{cell.value.map(x => x.name).join(', ')}</td>;
+                                        }
+                                        return <td key={cell.id}>{cell.render("Cell")}</td>;
+                                    })}
+                                    <td>
+                                        <button
+                                            className='customButton'
+                                            onClick={() => onEdit(row.original)}
+                                        >
+                                            <FontAwesomeIcon icon={faPencil} inverse />
+                                        </button>
+                                        <button
+                                            className='customButton'
+                                            onClick={() => onDelete(row.original)}
+                                        >
+                                            <FontAwesomeIcon icon={faTrash} inverse />
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div className="tableFooter">
                 <div>
                     Página{' '}
                     <em>
@@ -99,9 +100,9 @@ export default function ReactTable({columns, data, title, onEdit, onDelete}) {
                     </button>
                 </div>
                 <select value={pageSize}
-                        onChange={e => {
-                            setPageSize(Number(e.target.value));
-                        }}>
+                    onChange={e => {
+                        setPageSize(Number(e.target.value));
+                    }}>
                     {[5, 10, 20].map(pageSize => (
                         <option key={pageSize} value={pageSize}>
                             Mostrar {pageSize}
