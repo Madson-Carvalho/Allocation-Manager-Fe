91399b55edee0578ebf038809945cccde0d4cdc4
GabriellMonteiro <gabrielbrava@outlook.com.br>
2024-12-01 20:40:32 -0300

    adicionando button de desalocar ao details card

--- src/components/detailsCard/DetailsCard.js
+++ src/components/detailsCard/DetailsCard.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import moment from 'moment';
 import './DetailsCard.css';
+import httpRemove from "../../utils/httpRequest/httpRemove";
 
 const DetailsCard = ({ selectedItem, onClose, position, isEmployee }) => {
   if (!selectedItem) return null;
 
+    const deallocate = (id) => {
+        httpRemove("allocations/deallocate-employee-the-project", id);
+    };
+
   return (
-    <div
-      className="details-card"
-      style={{
-        position: 'absolute',
-        top: position.top,
-        left: position.left,
-        transform: 'translate(-50%, -50%)',
-        zIndex: 1000
-      }}
-    >
-      <h3 className='details-title'>Detalhes da Alocação</h3>
-        <div className='details-body'>
-            <p><span>Projeto:</span> {selectedItem.project.name}</p>
-            <p><span>Colaborador:</span> {selectedItem.employee.name}</p>
-            <p><span>Horas alocadas nesse projeto por dia:</span> {selectedItem.allocatedHours} hr</p>
-            <p><span>Horas de trabalho por dia:</span> {selectedItem.employee.workInSeconds} hr</p>
-            <p><span>Total de horas
+      <div
+          className="details-card"
+          style={{
+              position: 'absolute',
+              top: position.top,
+              left: position.left,
+              transform: 'translate(-50%, -50%)',
+              zIndex: 1000
+          }}
+      >
+          <h3 className='details-title'>Detalhes da Alocação</h3>
+          <div className='details-body'>
+              <p><span>Projeto:</span> {selectedItem.project.name}</p>
+              <p><span>Colaborador:</span> {selectedItem.employee.name}</p>
+              <p><span>Horas alocadas nesse projeto por dia:</span> {selectedItem.allocatedHours} hr</p>
+              <p><span>Horas de trabalho por dia:</span> {selectedItem.employee.workInSeconds} hr</p>
+              <p><span>Total de horas
                 ociosas:</span> {selectedItem.employee.workInSeconds - selectedItem.allocatedHours} hr</p>
-            <p><span>Data de Início da alocação:</span> {moment(selectedItem.startDate).format("DD/MM/YYYY HH:mm")}</p>
-            <p><span>Data de Término da alocação:</span> {moment(selectedItem.endDate).format("DD/MM/YYYY HH:mm")}</p>
-            <p><span>Data de Início do projeto:</span> {moment(selectedItem.project.initialDate).format("DD/MM/YYYY HH:mm")}</p>
-            <p><span>Data de Término do
+              <p><span>Data de Início da alocação:</span> {moment(selectedItem.startDate).format("DD/MM/YYYY HH:mm")}
+              </p>
+              <p><span>Data de Término da alocação:</span> {moment(selectedItem.endDate).format("DD/MM/YYYY HH:mm")}</p>
+              <p>
+                  <span>Data de Início do projeto:</span> {moment(selectedItem.project.initialDate).format("DD/MM/YYYY HH:mm")}
+              </p>
+              <p><span>Data de Término do
                 projeto:</span> {moment(selectedItem.project.deliveryDate).format("DD/MM/YYYY HH:mm")}</p>
-        </div>
-        <button className='details-button' onClick={onClose}>Fechar</button>
-    </div>
+          </div>
+          <div className="details-container-buttons">
+              <button className='details-button delete' onClick={() => deallocate(selectedItem.id)}>Desalocar</button>
+              <button className='details-button' onClick={onClose}>Fechar</button>
+          </div>
+      </div>
   );
 };
 
